ef88c5944880cdc757833e517d72d30b0536e8b5
cesavendano@wl.k12.ia.us <cesavendano@wl.k12.ia.us>
2018-11-01 14:30:28 -0500

    collected memes

--- js/memes.js
+++ js/memes.js
@@ -1,1 +1,111 @@
-var memes = [];
+var memes = [
+
+    {
+        image: "caesar.jpg", 
+        title: "Julius Caesar",
+        categories: ["Rome","Halo"],
+        meme_id: "1"
+    },
+    {
+        image: "austria.jpg", 
+        title: "Austria",
+        categories: ["ww1"],
+        meme_id: "2"
+    },
+    {
+        image: "french.jpg", 
+        title: "Renault",
+        categories: [""],
+        meme_id: "3"
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: "4"
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+    {
+        image: "", 
+        title: "",
+        categories: [""],
+        meme_id: ""
+    },
+];

246d4a2568aeb3df703a0d5f469aa4a0229bda0e
YOUR FULL NAME <jossanchez@wl.k12.ia.us>
2018-11-12 13:32:41 -0600

    set up easter-egg file

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -1,6 +1,23 @@
+var button = document.getElementById("easter-egg");
+var keys = [];
+
+
 function handleInput(input) {
     console.log(input);
-
+    keys.push(input);
+    if(keys.length >= 5){
+        if(keys[keys.length-1] === "d"){
+            if(keys[keys.length-2] === "s"){
+                if(keys[keys.length-3] === "a"){
+                    if(keys[keys.length-4] === "w"){
+                        if(keys[keys.length-5] === "spacebar"){
+                            $("#easter-egg").classList.remove("hidden");
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 document.addEventListener('keyup',function(e) {
